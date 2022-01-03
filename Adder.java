c0f24a7b2c689a28f21b86e813653ce3804f0509
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 20:03:50 +0900

    fix carry, sum 메서드명 변경
    - myCarry(), mySum() -> carry(), sum() 으로 변경하였다

--- Adder.java
+++ Adder.java
@@ -4,7 +4,7 @@ public class Adder {
     public static void main(String[] args) {
         boolean bitA = true;
         boolean bitB = false;
-        boolean carry = true;
+        boolean carry = carry(true, true);
 
         System.out.println(Arrays.toString(fullAdder(bitA, bitB, carry)));
 
@@ -29,13 +29,13 @@ public class Adder {
     private static boolean[] halfAdder(boolean bitA, boolean bitB) {
         boolean[] answer = new boolean[2];
 
-        answer[0] = mySum(bitA, bitB);
-        answer[1] = myCarry(bitA, bitB);
+        answer[0] = sum(bitA, bitB);
+        answer[1] = carry(bitA, bitB);
         return answer;
     }
 
     // AND 의 결과와 같다
-    private static boolean myCarry(boolean bitA, boolean bitB) {
+    private static boolean carry(boolean bitA, boolean bitB) {
 
         if (bitA & bitB == true) {
             return true;
@@ -44,7 +44,7 @@ public class Adder {
     }
 
     // XOR 의 결과와 같다
-    private static boolean mySum(boolean bitA, boolean bitB) {
+    private static boolean sum(boolean bitA, boolean bitB) {
 
         if (bitA == bitB) {
             return false;
