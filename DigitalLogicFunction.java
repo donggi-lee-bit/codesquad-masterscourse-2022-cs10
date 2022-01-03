e3169ea61b5a5f4669f5fa85b737832760b1ea85
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 15:32:25 +0900

    Add 미션1 AND 함수를 구현했다
    - 논리 연산자만 활용하여 구현하라는 요구사항이 있었다
    - AND 논리 함수를 구현하기 위해 boolean 타입 변수를 & 로 비교해주었다
    - &로 비교하게되면 변수 두 개가 모두 true이면 결과가 true이다
    - 하나라도 false라면 결과는 false가 나오게 했다

---
+++ DigitalLogicFunction.java
@@ -0,0 +1,16 @@
+public class DigitalLogicFunction {
+    public static void main(String[] args) {
+
+        boolean bitA = true;
+        boolean bitB = true;
+        System.out.println(myAnd(bitA, bitB));
+    }
+
+    public static boolean myAnd(boolean a, boolean b) {
+
+        if (a & b == true) {
+            return true;
+        }
+        return false;
+    }
+}
