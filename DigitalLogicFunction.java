f72f6a33a6f54ae40805647973fc260fbb253846
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 16:27:15 +0900

    Add NAND 함수 구현했다
    - NAND 연산은 AND 연산과 반대로 작동한다
    - 변수 두 개가 모두 false이면 true를 반환하고 하나라도 false이면 true를
      반환한다

--- DigitalLogicFunction.java
+++ DigitalLogicFunction.java
@@ -30,6 +30,29 @@ public class DigitalLogicFunction {
         bitB = false;
         System.out.println("OR : " + myOr(bitA, bitB));
         System.out.println("============================");
+
+        bitA = true;
+        bitB = true;
+        System.out.println("NAND : " + myNand(bitA, bitB));
+
+        bitB = false;
+        System.out.println("NAND : " + myNand(bitA, bitB));
+
+        bitA = false;
+        bitB = true;
+        System.out.println("NAND : " + myNand(bitA, bitB));
+
+        bitB = false;
+        System.out.println("NAND : " + myNand(bitA, bitB));
+        System.out.println("============================");
+    }
+
+    private static boolean myNand(boolean bitA, boolean bitB) {
+
+        if (bitA & bitB == true) {
+            return false;
+        }
+        return true;
     }
 
     private static boolean myAnd(boolean bitA, boolean bitB) {
