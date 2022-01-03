ec77ebad13b22f01a8cf2769f1fdd9e4d03edca0
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 17:27:44 +0900

    Add 반쪽덧셈 구현했다
    - boolean 타입 배열에 각각 sum, carry 함수 반환 값을 넣어주었다
    - 값을 넣어줄때 배열의 인덱스를 하드코딩했는데 다른 방법이 떠오르지
      않는다

--- Adder.java
+++ Adder.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+
 public class Adder {
     public static void main(String[] args) {
         boolean bitA = true;
         boolean bitB = true;
 
-        myCarry(bitA, bitB);
+        System.out.println(Arrays.toString(halfAdder(bitA, bitB)));
 
+    }
+
+    private static boolean[] halfAdder(boolean bitA, boolean bitB) {
+        boolean[] answer = new boolean[2];
+
+        answer[0] = mySum(bitA, bitB);
+        answer[1] = myCarry(bitA, bitB);
+        return answer;
     }
 
     // AND 의 결과와 같다
