20dac1779f557f35ffd7eb4e44a117cc204547d7
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 16:14:07 +0900

    Add OR 함수 구현
    - | 연산자를 이용했다
    - OR 연산자는 한 쪽 값만 true이면 true를 반환한다

--- DigitalLogicFunction.java
+++ DigitalLogicFunction.java
@@ -3,12 +3,46 @@ public class DigitalLogicFunction {
         boolean bitA = true;
         boolean bitB = true;
 
-        System.out.println(myAdd(bitA, bitB));
+        System.out.println("AND : " + myAnd(bitA, bitB));
+
+        bitB = false;
+        System.out.println("AND : " + myAnd(bitA, bitB));
+
+        bitA = false;
+        bitB = true;
+        System.out.println("AND : " + myAnd(bitA, bitB));
+
+        bitB = false;
+        System.out.println("AND : " + myAnd(bitA, bitB));
+        System.out.println("============================");
+
+        bitA = true;
+        bitB = true;
+        System.out.println("OR : " + myOr(bitA, bitB));
+
+        bitB = false;
+        System.out.println("OR : " + myOr(bitA, bitB));
+
+        bitA = false;
+        bitB = true;
+        System.out.println("OR : " + myOr(bitA, bitB));
+
+        bitB = false;
+        System.out.println("OR : " + myOr(bitA, bitB));
+        System.out.println("============================");
     }
 
-    private static boolean myAdd(boolean bitA, boolean bitB) {
+    private static boolean myAnd(boolean bitA, boolean bitB) {
 
         if (bitA & bitB == true) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean myOr(boolean bitA, boolean bitB) {
+
+        if (bitA | bitB == true) {
             return true;
         }
         return false;
