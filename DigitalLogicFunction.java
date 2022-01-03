694006900c7ef78c49662c08608abc14807167f2
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-03 16:31:50 +0900

    Add XOR 함수 구현했다
    - XOR 연산은 두 값이 서로 같으면 false, 다르면 true를 반환한다

--- DigitalLogicFunction.java
+++ DigitalLogicFunction.java
@@ -45,6 +45,29 @@ public class DigitalLogicFunction {
         bitB = false;
         System.out.println("NAND : " + myNand(bitA, bitB));
         System.out.println("============================");
+
+        bitA = true;
+        bitB = true;
+        System.out.println("XOR : " + myXor(bitA, bitB));
+
+        bitB = false;
+        System.out.println("XOR : " + myXor(bitA, bitB));
+
+        bitA = false;
+        bitB = true;
+        System.out.println("XOR : " + myXor(bitA, bitB));
+
+        bitB = false;
+        System.out.println("XOR : " + myXor(bitA, bitB));
+
+    }
+
+    private static boolean myXor(boolean bitA, boolean bitB) {
+
+        if (bitA == bitB) {
+            return false;
+        }
+        return true;
     }
 
     private static boolean myNand(boolean bitA, boolean bitB) {
