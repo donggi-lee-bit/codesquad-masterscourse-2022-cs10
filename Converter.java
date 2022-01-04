ad8b3859c79af5d3c6b77fa29f5b4ae3ac22bfcc
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-04 22:44:40 +0900

    Add bin2dec 구현
    - 포문을 돌면서 boolean 을 int로 변환시켜준다
    - 변환된 bin 값 중 0번째 인덱스가 true 였으면 answer에 1을 더해준다
    - 0번째 인덱스만 따로해주고 나머지 1번째 인덱스부터는 pow() 를
      거치게된다
    - 지수 계산을 거친 값을 answer에 더해주고 반환해준다

--- Converter.java
+++ Converter.java
@@ -1,9 +1,34 @@
 import java.util.ArrayList;
 
 public class Converter {
+    
+    public int bin2dec(boolean[] bin) {
+        int answer = 0;
+        int binary = 2;
 
-    DigitalLogicFunction dlf = new DigitalLogicFunction();
-    Adder ad = new Adder();
+        for (int i = 0; i < bin.length; i++) {
+            int temp = boolToInt(bin[i]);
+
+            if (i == 0 && temp == 1) {
+                answer += 1;
+            }
+
+            if (temp == 1 && i >= 1) {
+                answer += pow(binary, i);
+            }
+            
+        }
+        return answer;
+    }
+
+    private int pow(int base, int expo) {
+        int result = base;
+        for (int i = 1; i < expo; i++) {
+           result *= base;
+        }
+        return result;
+    }
+
 
     public boolean[] dec2bin(int decimal) {
         int i = 0;
@@ -18,7 +43,14 @@ public class Converter {
         return answer;
     }
 
-    public boolean intToBool(int remainder) {
+    private int boolToInt(boolean bin) {
+        if (bin) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private boolean intToBool(int remainder) {
         if (remainder == 0) {
             return false;
         }
