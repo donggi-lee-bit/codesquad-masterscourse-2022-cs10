7cf8927ac8f3cfaf0d64fbaac81d0000c3138c9c
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-04 21:45:22 +0900

    Fix dec2bin 수정
    - 배열 크기를 9로 변경해주었다 최대값인 256을 2진법으로 변환했을때
      필요한 배열이 9개여서 변경했다
    - while 조건문을 decimal이 0보다 클 때로 변경했다
    - while문이 끝나고 따로 true를 추가해주는 코드는 지웠따

--- Converter.java
+++ Converter.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Converter {
 
+    DigitalLogicFunction dlf = new DigitalLogicFunction();
+    Adder ad = new Adder();
+
+    public boolean[] dec2bin(int decimal) {
+        int i = 0;
+        boolean[] answer = new boolean[9];
+
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            decimal /= 2;
+            answer[i] = intToBool(remainder);
+            i++;
+        }
+        return answer;
+    }
+
+    public boolean intToBool(int remainder) {
+        if (remainder == 0) {
+            return false;
+        }
+        return true;
+    }
 }
